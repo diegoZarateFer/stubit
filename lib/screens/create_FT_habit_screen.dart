71ccf14147472136d21afdcdad0c2c871ebba2e8
Diego Zárate <diegozaratefernandez300@gmail.com>
2025-01-16 12:22:01 -0600

    Inicio de la implementación de la pantalla para crear un hábito medido por frecuencia y tiempo.

--- lib/screens/create_FT_habit_screen.dart
+++ lib/screens/create_FT_habit_screen.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:day_picker/day_picker.dart';
+
+final List<DayInWeek> _days = [
+  DayInWeek("D", dayKey: "monday"),
+  DayInWeek("L", dayKey: "tuesday"),
+  DayInWeek("M", dayKey: "wednesday"),
+  DayInWeek("M", dayKey: "thursday"),
+  DayInWeek("J", dayKey: "friday"),
+  DayInWeek("V", dayKey: "saturday", isSelected: true),
+  DayInWeek("S", dayKey: "sunday", isSelected: true),
+];
 
 class CreateFtHabitScreen extends StatefulWidget {
   const CreateFtHabitScreen({super.key});
@@ -46,6 +57,101 @@ class _CreateFtHabitScreenState extends State<CreateFtHabitScreen> {
                     ),
                     const SizedBox(
                       height: 16,
+                    ),
+                    Image.asset(
+                      "assets/images/calendar.png",
+                      height: 80,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Text(
+                      "Realizar estiramientos al despertar.",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Text(
+                      "Duración del hábito",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.dmSans(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Text(
+                      "¿Que días quieres realizar el hábito?",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.dmSans(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    SelectWeekDays(
+                      fontSize: 16,
+                      onSelect: (value) {},
+                      days: _days,
+                      selectedDaysFillColor: const Color(0xFFA6A6A6),
+                      unselectedDaysBorderColor: Colors.black,
+                      unselectedDaysFillColor: const Color(0xFFA557E8),
+                      selectedDaysBorderColor: Colors.black,
+                      selectedDayTextColor: Colors.white,
+                      unSelectedDayTextColor: Colors.white,
+                      borderWidth: 4,
+                      backgroundColor: Colors.transparent,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        backgroundColor: const Color.fromRGBO(121, 30, 198, 1),
+                      ),
+                      child: Text(
+                        "Aceptar",
+                        style: GoogleFonts.openSans(
+                          color: Colors.white,
+                          decorationColor: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        backgroundColor: Colors.white,
+                      ),
+                      child: Text(
+                        "Cancelar",
+                        style: GoogleFonts.openSans(
+                          color: Colors.black,
+                          decorationColor: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
                     ),
                   ],
                 ),
