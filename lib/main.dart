44b443575326315a337fc60479005d84b4786b16
Diego Zárate <diegozaratefernandez300@gmail.com>
2025-01-19 12:17:15 -0600

    Cambio en el widget main.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:stubit/firebase_options.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:stubit/screens/auth_wrapper.dart';
-import 'package:stubit/screens/create_FT_habit_screen.dart';
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -56,7 +55,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Stu-bit',
       theme: theme,
-      home: const CreateFtHabitScreen(),
+      home: const AuthWrapper(),
     );
   }
 }
