2c673d1cd85cbad00b5309e8df700f9bc6aefb9e
Diego Zárate <diegozaratefernandez300@gmail.com>
2025-02-10 09:57:50 -0600

    Acomodo de los widgets de la pantalla.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:stubit/firebase_options.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:stubit/screens/auth_wrapper.dart';
-import 'package:stubit/screens/edit_habits_screens/edit_habit_L_screen.dart';
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -56,7 +55,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Stu-bit',
       theme: theme,
-      home: const EditHabitLScreen(),
+      home: const AuthWrapper(),
     );
   }
 }
