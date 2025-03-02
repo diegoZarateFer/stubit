731b7b28c1a37912abe03d2d883947f1299879b9
Diego Zárate <diegozaratefernandez300@gmail.com>
2025-03-02 14:45:03 -0600

    Creación de widget para diálogo para los libros virtuales.

---
+++ lib/widgets/gems_dialog.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class GemsDialog extends StatelessWidget {
+  const GemsDialog({
+    super.key,
+    required this.title,
+    required this.message,
+  });
+
+  final String title, message;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      title: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: GoogleFonts.poppins(
+          fontSize: 16,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            message,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.dmSans(
+              fontSize: 14,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              backgroundColor: const Color.fromRGBO(121, 30, 198, 1),
+            ),
+            child: Text(
+              "Aceptar",
+              style: GoogleFonts.openSans(
+                color: Colors.white,
+                decorationColor: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
