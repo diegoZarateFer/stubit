7f4545c975da16becd6aa55f9fc13bf6a90364b1
Diego Zárate <diegozaratefernandez300@gmail.com>
2025-03-05 15:24:44 -0600

    Mostrando los días que se han registrado.

--- lib/screens/track_habit_screen.dart
+++ lib/screens/track_habit_screen.dart
@@ -1,22 +1,59 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:stubit/models/habit.dart';
+import 'package:stubit/widgets/apology.dart';
 import 'package:stubit/widgets/calendar.dart';
 import 'package:stubit/widgets/image_button.dart';
 import 'package:stubit/widgets/user_button.dart';
 
+FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
 class TrackHabitScreen extends StatefulWidget {
   const TrackHabitScreen({
     super.key,
+    required this.habit,
   });
+
+  final Habit habit;
 
   @override
   State<TrackHabitScreen> createState() => _TrackHabitScreenState();
 }
 
 class _TrackHabitScreenState extends State<TrackHabitScreen> {
+  User? _currentUser;
+
   @override
   void initState() {
     super.initState();
+    _currentUser = FirebaseAuth.instance.currentUser;
+  }
+
+  Future<Set<DateTime>?> fetchLogDates() async {
+    final userId = _currentUser!.uid.toString();
+    Set<DateTime> logDates = {};
+    try {
+      CollectionReference logRef = _firestore
+          .collection("user_data")
+          .doc(userId)
+          .collection("habits")
+          .doc(widget.habit.id)
+          .collection("habit_log");
+
+      var snapshot = await logRef.get();
+      for (var doc in snapshot.docs) {
+        if (doc.id != "daily_form") {
+          DateTime logDate = DateTime.parse(doc.id);
+          logDates.add(logDate);
+        }
+      }
+
+      return logDates;
+    } catch (e) {
+      throw Exception('Falló la eliminación del LOG.');
+    }
   }
 
   @override
@@ -52,74 +89,97 @@ class _TrackHabitScreenState extends State<TrackHabitScreen> {
           const UserButton(),
         ],
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color.fromRGBO(139, 34, 227, 1),
-              Colors.black,
-            ],
-          ),
-        ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 16,
-              horizontal: 8,
-            ),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Image.asset(
-                    "assets/images/calendar.png",
-                    height: 60,
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Text(
-                    "Lectura",
-                    style: GoogleFonts.poppins(
-                      fontSize: 18,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  const Calendar(),
-                  const SizedBox(height: 16),
-                  Container(
-                    width: 60,
-                    height: 80,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/fire.png"),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          "100",
-                          style: GoogleFonts.koHo(
-                            fontSize: 26,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+      body: FutureBuilder(
+        future: fetchLogDates(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return const Center(
+              child: Apology(
+                message:
+                    "Lo sentimos, no se ha podido cargar tu información. Inténtalo de nuevo más tarde.",
+              ),
+            );
+          }
+
+          final logDates = snapshot.data!;
+          return Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromRGBO(139, 34, 227, 1),
+                  Colors.black,
                 ],
               ),
             ),
-          ),
-        ),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 16,
+                  horizontal: 8,
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        "assets/images/calendar.png",
+                        height: 60,
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        "Lectura",
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Calendar(
+                        dates: logDates,
+                      ),
+                      const SizedBox(height: 16),
+                      Container(
+                        width: 60,
+                        height: 80,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/images/fire.png"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "100",
+                              style: GoogleFonts.koHo(
+                                fontSize: 26,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
--- lib/widgets/habit_item.dart
+++ lib/widgets/habit_item.dart
@@ -1,258 +1,260 @@
-  import 'package:cloud_firestore/cloud_firestore.dart';
-  import 'package:firebase_auth/firebase_auth.dart';
-  import 'package:flutter/material.dart';
-  import 'package:google_fonts/google_fonts.dart';
-  import 'package:stubit/models/habit.dart';
-  import 'package:stubit/screens/register_habits_screens/register_habit.dart';
-  import 'package:stubit/screens/track_habit_screen.dart';
-  import 'package:stubit/util/util.dart';
-  import 'package:stubit/widgets/confirmation_dialog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:stubit/models/habit.dart';
+import 'package:stubit/screens/register_habits_screens/register_habit.dart';
+import 'package:stubit/screens/track_habit_screen.dart';
+import 'package:stubit/util/util.dart';
+import 'package:stubit/widgets/confirmation_dialog.dart';
 
-  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  class HabitItem extends StatefulWidget {
-    const HabitItem({
-      super.key,
-      required this.habit,
-      required this.habitParameters,
-    });
+class HabitItem extends StatefulWidget {
+  const HabitItem({
+    super.key,
+    required this.habit,
+    required this.habitParameters,
+  });
 
-    final Habit habit;
-    final Map<String, dynamic> habitParameters;
+  final Habit habit;
+  final Map<String, dynamic> habitParameters;
 
-    @override
-    State<HabitItem> createState() => _HabitItemState();
+  @override
+  State<HabitItem> createState() => _HabitItemState();
+}
+
+class _HabitItemState extends State<HabitItem> {
+  bool _isCompleted = false;
+  final _currentUser = FirebaseAuth.instance.currentUser!;
+
+  Future<void> _loadHabitData() async {
+    final userId = _currentUser.uid.toString();
+    final date = getDateAsString();
+    final doc = await _firestore
+        .collection("user_data")
+        .doc(userId)
+        .collection("habits")
+        .doc(widget.habit.id)
+        .collection("habit_log")
+        .doc(date)
+        .get();
+
+    if (doc.exists) {
+      setState(() {
+        _isCompleted = true;
+      });
+    }
   }
 
-  class _HabitItemState extends State<HabitItem> {
-    bool _isCompleted = false;
-    final _currentUser = FirebaseAuth.instance.currentUser!;
+  String _getRegisterHabitText() {
+    if (widget.habit.strategy == 'TP') {
+      return _isCompleted ? "Agregar ciclo" : "Registrar";
+    }
 
-    Future<void> _loadHabitData() async {
-      final userId = _currentUser.uid.toString();
-      final date = getDateAsString();
-      final doc = await _firestore
+    if (widget.habit.strategy == 'COF') {
+      return "Agregar a registro";
+    }
+    return _isCompleted ? "Modificar registro" : "Registrar";
+  }
+
+  Icon _getRegisterHabitIcon() {
+    if (widget.habit.strategy == 'TP') {
+      return Icon(_isCompleted ? Icons.add : Icons.check);
+    }
+
+    if (widget.habit.strategy == 'COF') {
+      return const Icon(Icons.add);
+    }
+    return Icon(_isCompleted ? Icons.edit_calendar_outlined : Icons.check);
+  }
+
+  Future<void> _deleteHabitLog(String userId) async {
+    try {
+      CollectionReference logRef = _firestore
           .collection("user_data")
           .doc(userId)
           .collection("habits")
           .doc(widget.habit.id)
-          .collection("habit_log")
-          .doc(date)
-          .get();
+          .collection("habit_log");
 
-      if (doc.exists) {
-        setState(() {
-          _isCompleted = true;
-        });
+      var snapshot = await logRef.get();
+      for (var doc in snapshot.docs) {
+        await doc.reference.delete();
       }
-    }
-
-    String _getRegisterHabitText() {
-      if (widget.habit.strategy == 'TP') {
-        return _isCompleted ? "Agregar ciclo" : "Registrar";
-      }
-
-      if (widget.habit.strategy == 'COF') {
-        return "Agregar a registro";
-      }
-      return _isCompleted ? "Modificar registro" : "Registrar";
-    }
-
-    Icon _getRegisterHabitIcon() {
-      if (widget.habit.strategy == 'TP') {
-        return Icon(_isCompleted ? Icons.add : Icons.check);
-      }
-
-      if (widget.habit.strategy == 'COF') {
-        return const Icon(Icons.add);
-      }
-      return Icon(_isCompleted ? Icons.edit_calendar_outlined : Icons.check);
-    }
-
-    Future<void> _deleteHabitLog(String userId) async {
-      try {
-        CollectionReference logRef = _firestore
-            .collection("user_data")
-            .doc(userId)
-            .collection("habits")
-            .doc(widget.habit.id)
-            .collection("habit_log");
-
-        var snapshot = await logRef.get();
-        for (var doc in snapshot.docs) {
-          await doc.reference.delete();
-        }
-      } catch (e) {
-        throw Exception('Falló la eliminación del LOG.');
-      }
-    }
-
-    void _showMenuAction(BuildContext context) {
-      showModalBottomSheet(
-        context: context,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            top: Radius.circular(16),
-          ),
-        ),
-        builder: (ctx) {
-          final registerHabitText = _getRegisterHabitText();
-          final registerHabitIcon = _getRegisterHabitIcon();
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: Text(
-                  widget.habit.name,
-                  style: GoogleFonts.poppins(
-                    color: Colors.white,
-                    fontSize: 14,
-                  ),
-                ),
-              ),
-              const Divider(),
-              ListTile(
-                leading: registerHabitIcon,
-                title: Text(registerHabitText),
-                onTap: () async {
-                  await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (ctx) => RegisterHabit(
-                        habit: widget.habit,
-                        habitParameters: widget.habitParameters,
-                      ),
-                    ),
-                  );
-                  await _loadHabitData();
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.calendar_month),
-                title: const Text('Seguimiento'),
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (ctx) => const TrackHabitScreen(),
-                    ),
-                  );
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.edit),
-                title: const Text('Editar hábito'),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: const Icon(
-                  Icons.delete,
-                  color: Colors.red,
-                ),
-                title: const Text(
-                  'Eliminar hábito',
-                  style: TextStyle(
-                    color: Colors.red,
-                  ),
-                ),
-                onTap: () async {
-                  bool deleteConfirmation = await showConfirmationDialog(
-                        ctx,
-                        "Eliminar hábito",
-                        "Se borrará toda la información de tu hábito permanentemente.",
-                        "Eliminar",
-                        "Cancelar",
-                      ) ??
-                      false;
-
-                  ScaffoldMessenger.of(context).clearSnackBars();
-                  final userId = _currentUser.uid.toString();
-                  if (deleteConfirmation) {
-                    Navigator.of(ctx).pop();
-                    final rootContext =
-                        Navigator.of(context, rootNavigator: true).context;
-                    try {
-                      await _deleteHabitLog(userId);
-                      await _firestore
-                          .collection("user_data")
-                          .doc(userId)
-                          .collection("habits")
-                          .doc(widget.habit.id)
-                          .delete();
-
-                      ScaffoldMessenger.of(rootContext).showSnackBar(
-                        const SnackBar(
-                          content: Text(
-                            'Se ha eliminado el hábito.',
-                          ),
-                          duration: const Duration(seconds: 3),
-                        ),
-                      );
-                    } catch (e) {
-                      ScaffoldMessenger.of(rootContext).showSnackBar(
-                        const SnackBar(
-                          content: Text(
-                            'No se ha podido completar la acción. Por favor, inténtalo más tarde',
-                          ),
-                        ),
-                      );
-                    }
-                  }
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
-
-    @override
-    void initState() {
-      super.initState();
-      _loadHabitData();
-    }
-
-    @override
-    Widget build(BuildContext context) {
-      return GestureDetector(
-        onTap: () {
-          _showMenuAction(context);
-        },
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          margin: const EdgeInsets.only(
-            bottom: 16,
-          ),
-          decoration: BoxDecoration(
-            color: const Color(0xFF292D39),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Row(
-            children: [
-              Expanded(
-                child: Text(
-                  textAlign: TextAlign.center,
-                  widget.habit.name,
-                  softWrap: true,
-                  maxLines: 2,
-                  style: GoogleFonts.poppins(
-                    color: Colors.white,
-                    fontSize: 14,
-                    decoration: _isCompleted ? TextDecoration.lineThrough : null,
-                    decorationColor: Colors.white,
-                    decorationThickness: 3,
-                  ),
-                ),
-              ),
-              Icon(
-                Icons.local_fire_department,
-                color: _isCompleted ? Colors.amber : Colors.grey,
-                size: 32,
-              ),
-            ],
-          ),
-        ),
-      );
+    } catch (e) {
+      throw Exception('Falló la eliminación del LOG.');
     }
   }
+
+  void _showMenuAction(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(16),
+        ),
+      ),
+      builder: (ctx) {
+        final registerHabitText = _getRegisterHabitText();
+        final registerHabitIcon = _getRegisterHabitIcon();
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                widget.habit.name,
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            const Divider(),
+            ListTile(
+              leading: registerHabitIcon,
+              title: Text(registerHabitText),
+              onTap: () async {
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (ctx) => RegisterHabit(
+                      habit: widget.habit,
+                      habitParameters: widget.habitParameters,
+                    ),
+                  ),
+                );
+                await _loadHabitData();
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.calendar_month),
+              title: const Text('Seguimiento'),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (ctx) => TrackHabitScreen(
+                      habit: widget.habit,
+                    ),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.edit),
+              title: const Text('Editar hábito'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+              title: const Text(
+                'Eliminar hábito',
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+              onTap: () async {
+                bool deleteConfirmation = await showConfirmationDialog(
+                      ctx,
+                      "Eliminar hábito",
+                      "Se borrará toda la información de tu hábito permanentemente.",
+                      "Eliminar",
+                      "Cancelar",
+                    ) ??
+                    false;
+
+                ScaffoldMessenger.of(context).clearSnackBars();
+                final userId = _currentUser.uid.toString();
+                if (deleteConfirmation) {
+                  Navigator.of(ctx).pop();
+                  final rootContext =
+                      Navigator.of(context, rootNavigator: true).context;
+                  try {
+                    await _deleteHabitLog(userId);
+                    await _firestore
+                        .collection("user_data")
+                        .doc(userId)
+                        .collection("habits")
+                        .doc(widget.habit.id)
+                        .delete();
+
+                    ScaffoldMessenger.of(rootContext).showSnackBar(
+                      const SnackBar(
+                        content: Text(
+                          'Se ha eliminado el hábito.',
+                        ),
+                        duration: const Duration(seconds: 3),
+                      ),
+                    );
+                  } catch (e) {
+                    ScaffoldMessenger.of(rootContext).showSnackBar(
+                      const SnackBar(
+                        content: Text(
+                          'No se ha podido completar la acción. Por favor, inténtalo más tarde',
+                        ),
+                      ),
+                    );
+                  }
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadHabitData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _showMenuAction(context);
+      },
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        margin: const EdgeInsets.only(
+          bottom: 16,
+        ),
+        decoration: BoxDecoration(
+          color: const Color(0xFF292D39),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(
+                textAlign: TextAlign.center,
+                widget.habit.name,
+                softWrap: true,
+                maxLines: 2,
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 14,
+                  decoration: _isCompleted ? TextDecoration.lineThrough : null,
+                  decorationColor: Colors.white,
+                  decorationThickness: 3,
+                ),
+              ),
+            ),
+            Icon(
+              Icons.local_fire_department,
+              color: _isCompleted ? Colors.amber : Colors.grey,
+              size: 32,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
